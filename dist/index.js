fd0ed2da4e5272468761e455bd51599229ab817d
nateschutte <nate@ignidus.com>
2023-06-15 14:24:41 -0700

    - package app with node 16

--- dist/index.js
+++ dist/index.js
@@ -1388,14 +1388,36 @@ const core = __webpack_require__(470);
 const tmp = __webpack_require__(150);
 const fs = __webpack_require__(747);
 
+function parseEnvironmentVariables(environmentVariables) {
+  const vars = [];
+
+  environmentVariables.split('\n').forEach(line => {
+    const trimmedLine = line.trim();
+    if (trimmedLine.length === 0) { return; }
+    
+    const separatorIdx = trimmedLine.indexOf("=");
+
+    if (separatorIdx === -1) {
+        throw new Error(`Cannot parse the environment variable '${trimmedLine}'. Environment variable pairs must be of the form NAME=value.`);
+    }
+
+    vars.push({
+      name: trimmedLine.substring(0, separatorIdx),
+      value: trimmedLine.substring(separatorIdx + 1),
+    });
+  });
+
+  return vars;
+}
+
 async function run() {
   try {
     // Get inputs
     const taskDefinitionFile = core.getInput('task-definition', { required: true });
     const containerName = core.getInput('container-name', { required: true });
     const imageURI = core.getInput('image', { required: true });
-
     const environmentVariables = core.getInput('environment-variables', { required: false });
+    const environmentFiles = core.getInput('environment-files', { required: false });
 
     // Parse the task definition
     const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
@@ -1418,43 +1440,42 @@ async function run() {
     }
     containerDef.image = imageURI;
 
-    if (environmentVariables) {
+    const envVars = [];
 
-      // If environment array is missing, create it
+    // Apply environment variables from each file specified in order
+    if (environmentFiles) {
+      environmentFiles.split('\n').forEach(line => {
+        const trimmedLine = line.trim();
+        if (trimmedLine.length === 0) { return; }
+
+        if (!fs.existsSync(trimmedLine)) {
+          throw new Error(`Environment file not found: ${trimmedLine}`);
+        }
+
+        const fileVariables = fs.readFileSync(trimmedLine);
+
+        envVars.push(...parseEnvironmentVariables(fileVariables));
+      });
+    }
+
+    // Apply environment variables explicitly written, overwriting same variables from files
+    if (environmentVariables) {
+      envVars.push(...parseEnvironmentVariables(environmentVariables));
+    }
+
+    if (envVars) {
       if (!Array.isArray(containerDef.environment)) {
         containerDef.environment = [];
       }
-
-      // Get pairs by splitting on newlines
-      environmentVariables.split('\n').forEach(function (line) {
-        // Trim whitespace
-        const trimmedLine = line.trim();
-        // Skip if empty
-        if (trimmedLine.length === 0) { return; }
-        // Split on =
-        const separatorIdx = trimmedLine.indexOf("=");
-        // If there's nowhere to split
-        if (separatorIdx === -1) {
-            throw new Error(`Cannot parse the environment variable '${trimmedLine}'. Environment variable pairs must be of the form NAME=value.`);
-        }
-        // Build object
-        const variable = {
-          name: trimmedLine.substring(0, separatorIdx),
-          value: trimmedLine.substring(separatorIdx + 1),
-        };
-
-        // Search container definition environment for one matching name
+      envVars.forEach(variable => {
         const variableDef = containerDef.environment.find((e) => e.name == variable.name);
         if (variableDef) {
-          // If found, update
           variableDef.value = variable.value;
         } else {
-          // Else, create
           containerDef.environment.push(variable);
         }
       })
     }
-
 
     // Write out a new task definition file
     var updatedTaskDefFile = tmp.fileSync({
